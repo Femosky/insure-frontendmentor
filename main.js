def8009235a96e7356f5e68ba303f7940d23638e
Femosky <31240841+Femosky@users.noreply.github.com>
2023-05-19 19:09:17 +0000

    remaining backgrounds

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-nav");
+const header = document.querySelector(".primary-header");
 
 navToggle.addEventListener("click", () => {
   primaryNav.hasAttribute("data-visible")
     ? navToggle.setAttribute("aria-expanded", false)
     : navToggle.setAttribute("aria-expanded", true);
   primaryNav.toggleAttribute("data-visible");
+  header.toggleAttribute("data-visible");
 });
